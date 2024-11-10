a305bd0244cd674485da33dbc8914a2082435756
WhatsWrongHere <zaboradaniil@gmail.com>
2024-11-10 18:59:20 +0200

    Update setup.py
    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dmbreg",
-    version="0.1.0",
+    version="0.1.1",
     description="A library of simple regression models.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
